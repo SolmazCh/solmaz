a9f7f0b6078aef5d05c1075bfcdf850a79a49c61
SolmazCh <solmaz.chiraqova@gmail.com>
2020-05-09 12:25:44 -0400

    update Practice.java

--- src/day_1/Practice.java
+++ src/day_1/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello Solmaz");
         System.out.println("yes"); // I dont have any issue with Git or Github
+        System.out.println("OK great");
     }
 }
