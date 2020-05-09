beff8fdeee898e6519175da0de0516ab16df2973
SolmazCh <64098166+SolmazCh@users.noreply.github.com>
2020-05-09 12:22:30 -0400

    Update Practice.java

--- src/day_1/Practice.java
+++ src/day_1/Practice.java
@@ -3,6 +3,6 @@ package day_1;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello Solmaz");
-        System.out.println("yes");
+        System.out.println("yes"); // I dont have any issue with Git or Github
     }
 }
